bb3cd8389af0881bf1cad978e28af24702ed81aa
Andreas Lerke <andreas@googlemail.com>
2026-02-09 14:13:58 +0100

    Pydantic schemas bestimmt

---
+++ app/models/schemas.py
@@ -0,0 +1,46 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+# === 1.INVENTORY SCHEMAS ===
+
+# === 1.1 SCHEMA ANLAGE DES INVENTORY-EINTRAGS ===
+class InventoryCreate(BaseModel):
+    product_name: str
+    quantitiy: int
+    price: float
+    category: str
+
+# === 1.2 SCHEMA API-ANTWORT(INKL. ID + TIMESTAMP) ===
+class InventoryResponse(BaseModel):
+    id: int
+    product_name: str
+    quantity: int
+    price: float
+    category: str
+    last_updated = datetime
+
+    class Config:
+        from_attributes = True
+
+# === 2.SUPPLIER SCHMEAS ===
+
+# === 2.1 SCHEMA ANLAGE DES SUPPLIERS-EINTRAGS ===
+class SupplierCreate(BaseModel):
+    name: str
+    delivery_reliability: float
+    avg_delivery_days: float
+    price_level = float
+    quality_score = float
+
+# === 2.2 SCHEMA FÜR DIE API-ANTWORT ===
+class SupplierResponse(BaseModel):
+    id: int
+    name: str
+    delivery_reliability: float
+    avg_delivery_days: float
+    price_level: float
+    quality_score:float
+
+    class Config:
+        from_attributes = True
+
